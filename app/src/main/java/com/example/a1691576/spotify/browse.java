bf3e1a4ef802f82120e8277f313be07eb04a91fc
jyoti <jyotssharma1615@gmail.com>
2017-05-20 16:40:26 -0400

    homepage

---
+++ app/src/main/java/com/example/a1691576/spotify/browse.java
@@ -0,0 +1,13 @@
+package com.example.a1691576.spotify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class browse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse);
+    }
+}
