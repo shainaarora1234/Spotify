93fc697dbd2248a69c6928d56b0624a1019c85d3
Jyoti <jyotssharma1615@gmail.com>
2017-05-21 09:39:12 -0400

    123

--- app/src/main/java/com/example/a1691576/spotify/Charts.java
+++ app/src/main/java/com/example/a1691576/spotify/Charts.java
@@ -1,7 +1,9 @@
 package com.example.a1691576.spotify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Charts extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Charts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.charts);
+
+    }
+    public void charts(View view) {
+        Intent intent = new Intent(this, Charts.class);
+        startActivity(intent);
     }
 }
